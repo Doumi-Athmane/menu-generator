5b1c9ee9832eb3f3a7e2a519a52e9764b27c69f1
nabil-otsmane <hn_otsmane@esi.dz>
2020-11-18 11:05:53 +0100

    add table

--- frontend/src/components/plats/index.js
+++ frontend/src/components/plats/index.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Table from './tableau'
 import Button from '../button'
+import './index.css'
+
+const tmpPlats = [
+    {id: 1, nom: "salade", type: "dessert", prix: "500 DA"},
+    {id: 2, nom: "salade", type: "dessert", prix: "500 DA"},
+    {id: 3, nom: "salade", type: "dessert", prix: "500 DA"},
+    {id: 4, nom: "salade", type: "dessert", prix: "500 DA"},
+    {id: 5, nom: "salade", type: "dessert", prix: "500 DA"},
+    {id: 6, nom: "salade", type: "dessert", prix: "500 DA"},
+    {id: 7, nom: "salade", type: "dessert", prix: "500 DA"},
+    {id: 8, nom: "salade", type: "dessert", prix: "500 DA"},
+]
 
 export default function Plats(props) {
     return (
-        <div>
-            <h3>Plats</h3>
-            <Table />
+        <div className="pagePlats">
             <div>
-                <Button label="Ajouter" />
+                <h3>Plats</h3>
+            </div>
+            <Table plats={tmpPlats} />
+            <div className="btnCont">
+                <Button label="Ajouter Nouveau" />
             </div>
         </div>
     )
--- frontend/src/components/plats/tableau.js
+++ frontend/src/components/plats/tableau.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Row from './row'
+import './tableau.css'
 
-export default function Tableau(props) {
+
+
+export default function Tableau({ plats }) {
     return (
-        <div>
-            this is the table
-        </div>
+        <table className="grid">
+            <tbody>
+                <tr>
+                    <th>Nom</th>
+                    <th>Type</th>
+                    <th>Prix</th>
+                    <th> </th>
+                    <th> </th>
+                </tr>
+                {plats && plats.map(e => (
+                    <Row nom={e.nom} type={e.type} prix={e.prix} key={e.id} />
+                ))}
+            </tbody>
+        </table>
     )
 }
